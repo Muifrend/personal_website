54c1150bd3709da91effb7e9a4855a0a84dae2e2
Muifrend <andrzejzadaniadomowe1@gmail.com>
2025-11-28 00:23:15 -0800

    feat: add GitHub Actions workflow for deploying to GitHub Pages and configure site settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],
-
+  site: 'https://andrzejfirek.me',
   vite: {
     plugins: [tailwindcss()]
   }
